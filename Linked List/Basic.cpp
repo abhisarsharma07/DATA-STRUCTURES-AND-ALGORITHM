16592130daeb450d275788debbadd56d13b05997
abhisarsharma07 <abhisarsharmajee@gmail.com>
2025-01-08 01:31:37 +0530

    commit

---
+++ Linked List/Basic.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+struct Node{
+     public://data types
+     int data;
+     Node* next;
+
+     public://constructor
+     Node(int data1,Node* next1){
+          data=data1;
+          next=next1;
+     }
+}
+
+int main(){
+
+     
+}
