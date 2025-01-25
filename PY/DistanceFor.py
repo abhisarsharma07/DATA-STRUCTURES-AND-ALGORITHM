b70a0e66fc4b1aa0be8cea558d337b07921af312
abhisarsharma07 <abhisarsharmajee@gmail.com>
2025-01-25 09:25:36 +0530

    d

---
+++ PY/DistanceFor.py
@@ -0,0 +1,8 @@
+x1=int(input("Enter the value of x1:"))
+x2=int(input("Enter the value of x2:"))
+y1=int(input("Enter the value of x3:"))
+y2=int(input("Enter the value of x4:"))
+d=((x1-x2)**2+(y1-y2)**2)**0.5
+#distance formula
+
+print(d)
