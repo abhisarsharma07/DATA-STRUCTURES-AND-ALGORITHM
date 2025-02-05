9744d8bded4ef0ff167da4d09a6656e8e44ad3b0
abhisarsharma07 <abhisarsharmajee@gmail.com>
2025-02-06 01:03:07 +0530

    Create PeakInMountainArray.cpp

---
+++ Questions/Searching/PeakInMountainArray.cpp
@@ -0,0 +1,19 @@
+//Good Concept
+class Solution {
+public:
+    int peakIndexInMountainArray(vector<int>& arr) {
+        int s=0;
+        int e=arr.size()-1;
+        int mid=s+(e-s)/2;
+        while(s<e){//do baar compare ho raha hai
+            if(arr[mid]<arr[mid+1]){
+                s=mid+1;
+            }
+            else{
+                e=mid;
+            }
+             mid=s+(e-s)/2;
+        }
+        return s;
+    }
+};
